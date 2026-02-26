b85b88b3ab9c81ac2febe7639b146fcc3c89b108
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-02-27 01:00:01 +0600

    first commit

---
+++ truthy.js
@@ -0,0 +1,18 @@
+let data=0;
+// data='';
+// data=true;
+// data=null;
+data='lokman';
+if(data){
+    console.log('truthy')
+}
+else{
+    console.log('falsey')
+}
+
+ 
+let userName = "Lokman";
+
+let isLoggedIn = !!userName;
+
+console.log(isLoggedIn); // true
---
+++ undifine.js
@@ -0,0 +1,16 @@
+let data;
+console.log(data)
+
+function add(a, b) {
+    let sum = a + b;
+}
+
+console.log(add(5, 3));
+
+
+
+let person = {
+    name: "Lokman"
+};
+
+console.log(person.age);
